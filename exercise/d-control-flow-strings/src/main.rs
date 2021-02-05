88e1d06f0757e5e9389714cf84dec89cdcdca7d2
Bora Guven <guvenbe@gmail.com>
2021-02-04 23:30:39 -0500

    data types

--- exercise/d-control-flow-strings/src/main.rs
+++ exercise/d-control-flow-strings/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
+
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
@@ -22,7 +23,16 @@ fn main() {
 
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
-        // after "cargo run".  For example "cargo run sum"
+        // after "cargo run".  For example "cargo run  -- sum"
+
+
+        if arg == "sum" {
+            sum();
+        } else if arg == "double"{
+            double();
+        } else {
+            count(arg);
+        }
     }
 }
 
@@ -32,6 +42,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for i in 7..=23{
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -43,7 +56,10 @@ fn double() {
     // by 2) before it is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
-
+    while x<=500{
+        x*=2;
+        count +=1
+    }
     println!("You can double x {} times before it is larger than 500", count);
 }
 
@@ -53,6 +69,14 @@ fn count(arg: String) {
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
 
+    let mut count:i32 =0;
+    loop {
+        if count <= 8 {
+            print!( "arg = {}", arg);
+            break;
+        }
+        count +=1;
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }

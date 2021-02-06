bcb61f573804e9f7ccb7d8d61060756bb4630836
Bora Guven <guvenbe@gmail.com>
2021-02-06 01:14:14 -0500

    loops

---
+++ strings/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    let s = String::from("initial contents");
+    //Unicode
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    //grow string
+    let mut s = String::from("foo");
+    //append
+    s.push_str("bar");
+
+    println!("{}" ,s);
+
+    let mut s = String::from("lo");
+    //add single character
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    // println!("{} , {}, {}, {}" , s, s1, s2, s3); // does not work
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{} , {}, {}, {}" , s, s1, s2, s3);
+
+}
